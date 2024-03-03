61afef2dcc8d1d883298e6b8e8ef5ed71a39616b
silai <silaichev1999@gmail.com>
2024-03-03 11:32:56 +0200

    Initial commit

---
+++ src/main/java/org/game/haven/GameHaven/GameHavenApplication.java
@@ -0,0 +1,13 @@
+package org.game.haven.GameHaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameHavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/game/haven/GameHaven/GameHavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.game.haven.GameHaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
